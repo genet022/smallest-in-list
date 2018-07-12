7d31bd60fcd85ba7cd10f39b1617e1ee5cb70320
Genet <joshua.s.genet@hpe.com>
2018-07-12 15:52:39 -0600

    Add basic readableGetSmallest function

--- smallest.go
+++ smallest.go
@@ -7,10 +7,18 @@ func main() {
     x := []int {
         42, 49, 86, 143,
         234, 334, 401, 435,
-        2, 14, 21
-    }
+        2, 14, 21}
 
-    fmt.Println("smallest from readable algorithm   : ", 0)
+    fmt.Println("smallest from readable algorithm   : ", readableGetSmallest(x))
     fmt.Println("smallest from performant algorithm : ", 0)
     fmt.Println("smallest from extendable algorithm : ", 0)
+}
+
+func readableGetSmallest(x []int) int {
+    var smallest int
+
+    sort.Ints(x)
+    smallest = x[0]
+
+    return smallest
 }
